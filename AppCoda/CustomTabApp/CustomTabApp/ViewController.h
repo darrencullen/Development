dc360e68f0bfcfaa0f8e73634d2bef14f1b12d05
DC <darrencullen@gmail.com>
2013-02-17 15:40:55 +0000

    Customise tab bar

---
+++ AppCoda/CustomTabApp/CustomTabApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomTabApp
+//
+//  Created by darren cullen on 17/02/2013.
+//  Copyright (c) 2013 appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AppCoda/CustomTabApp/CustomTabApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomTabApp
+//
+//  Created by darren cullen on 17/02/2013.
+//  Copyright (c) 2013 appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
