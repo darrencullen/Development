7da8d2cfd2d45b22222ae63b3966777f40b3c561
DC <darrencullen@gmail.com>
2013-02-17 15:19:49 +0000

    Storyboard tutorials

---
+++ AppCoda/RecipeBook/RecipeBook/AboutViewController.h
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.h
+//  RecipeBook
+//
+//  Created by darren cullen on 17/02/2013.
+//  Copyright (c) 2013 appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+
+@end
---
+++ AppCoda/RecipeBook/RecipeBook/AboutViewController.m
@@ -0,0 +1,41 @@
+//
+//  AboutViewController.m
+//  RecipeBook
+//
+//  Created by darren cullen on 17/02/2013.
+//  Copyright (c) 2013 appcoda. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:@"about.html" ofType:nil]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
