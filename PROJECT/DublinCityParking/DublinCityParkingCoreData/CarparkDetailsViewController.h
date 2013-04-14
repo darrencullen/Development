d1dde3fb593314d81cc3b96995749ce78d40c5c3
DC <darrencullen@gmail.com>
2013-04-14 20:55:36 +0100

    Carpark details screen

---
+++ PROJECT/DublinCityParking/DublinCityParkingCoreData/CarparkDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CarparkDetailsViewController.h
+//  DublinCityParking
+//
+//  Created by darren cullen on 14/04/2013.
+//  Copyright (c) 2013 dcdevstudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CarparkDetailsViewController : UITableViewController
+
+@end
